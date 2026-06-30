6ad1977c999bfacd2da4878c86c948e24875aed2
mrbaker1917 <mrbaker1917@gmail.com>
2026-06-29 17:23:15 -0700

    adds logger with multi-logging to file and stderr

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/signal"
@@ -26,20 +27,18 @@ func main() {
 }
 
 func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir string) int {
-	f, err := os.OpenFile("linko.access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	logFile := os.Getenv("LINKO_LOG_FILE")
+	logger, err := initializeLogger(logFile)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error in creating/opening linko.access.log")
+		fmt.Fprintf(os.Stderr, "failed to initialize logger: %w\n", err)
 		return 1
 	}
-	accessLogger := log.New(f, "INFO: ", log.LstdFlags)
-
-	standardLogger := log.New(os.Stderr, "DEBUG: ", log.LstdFlags)
-	st, err := store.New(dataDir, standardLogger)
+	st, err := store.New(dataDir, logger)
 	if err != nil {
-		standardLogger.Printf("failed to create store: %v", err)
+		logger.Printf("failed to create store: %v", err)
 		return 1
 	}
-	s := newServer(*st, httpPort, accessLogger, cancel)
+	s := newServer(*st, httpPort, logger, cancel)
 	var serverErr error
 	go func() {
 		serverErr = s.start()
@@ -48,14 +47,28 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 	<-ctx.Done()
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	standardLogger.Println("Linko is shutting down")
+	logger.Println("Linko is shutting down")
 	if err := s.shutdown(shutdownCtx); err != nil {
-		standardLogger.Printf("failed to shutdown server: %v", err)
+		logger.Printf("failed to shutdown server: %v", err)
 		return 1
 	}
 	if serverErr != nil {
-		standardLogger.Printf("server error: %v", serverErr)
+		logger.Printf("server error: %v", serverErr)
 		return 1
 	}
 	return 0
+}
+
+func initializeLogger(logFile string) (*log.Logger, error) {
+	if logFile != "" {
+		file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			return nil, fmt.Errorf("failed to open log file: %w", err)
+		}
+		multiWriter := io.MultiWriter(os.Stderr, file)
+		logger := log.New(multiWriter, "", log.LstdFlags)
+		return logger, nil
+	}
+	return log.New(os.Stderr, "", log.LstdFlags), nil
+
 }
